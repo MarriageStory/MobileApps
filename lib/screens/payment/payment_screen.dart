3bffe9b9799d5cd977937d735ca056f08f1bf800
iostream-code <aryakmifjannah2603@gmail.com>
2022-05-23 23:22:56 +0700

    menambah payment

---
+++ lib/screens/payment/payment_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+// import 'package:wedding_planner/screens/payment/components/body/';
+
+class PaymentScreen extends StatelessWidget {
+  const PaymentScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

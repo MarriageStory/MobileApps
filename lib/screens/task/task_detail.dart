4f942fad8fe88191fb262076c88ee90cfca9bf7a
efriza <Efrizals@it.student.pens.ac.id>
2022-06-09 13:08:55 +0700

    BEEEE

---
+++ lib/screens/task/task_detail.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:wedding_planner/screens/homePage/homePage.dart';
+import 'package:wedding_planner/model/scheduleModel.dart';
+
+class DetailTask extends StatelessWidget {
+  static final url = "/detail-task";
+  const DetailTask({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Schedules schedule =
+        ModalRoute.of(context)!.settings.arguments as Schedules;
+
+    return Scaffold(
+      body: Column(children: <Widget>[
+        const Padding(padding: EdgeInsets.all(18.0)),
+        const Padding(padding: EdgeInsets.only(top: 8)),
+        Container(
+          margin: const EdgeInsets.only(right: 16, left: 16),
+          height: 70,
+          child: Row(
+            children: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const homePage()),
+                    );
+                  },
+                  child: Icon(Icons.arrow_back)),
+              SizedBox(width: 76),
+              Text(
+                "Detail Task",
+                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+              ),
+              SizedBox(width: 70),
+              IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const homePage()),
+                    );
+                  },
+                  icon: Icon(Icons.list))
+            ],
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(right: 23, left: 23),
+          height: MediaQuery.of(context).size.height * 0.732,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 2,
+                offset: Offset(0, 0), // Shadow position
+              ),
+            ],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.only(right: 16, left: 16, top: 20),
+                child: Text(
+                  schedule.namaClient,
+                  style: TextStyle(color: Colors.black),
+                  textAlign: TextAlign.center,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Color(0xFFFE6A7E).withOpacity(0.65),
+                      Color(0xFFFE6A7E),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(right: 16, left: 16, top: 20),
+                child: Text(
+                  schedule.jam,
+                  style: TextStyle(fontSize: 35),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(right: 16, left: 16, top: 2),
+                child: Text(
+                  schedule.tanggal.toString(),
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(right: 16, left: 16, top: 30),
+                child: Text(
+                  "Task:",
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(right: 16, left: 16, top: 2),
+                child: Text(
+                  schedule.namaKegiatan,
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(right: 16, left: 16, top: 30),
+                child: Text(
+                  "Place:",
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(right: 16, left: 16, top: 2),
+                child: Text(
+                  schedule.tempat,
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(right: 16, left: 16, top: 30),
+                child: Text(
+                  "Detail:",
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(right: 16, left: 16, top: 2),
+                child: Text(
+                  schedule.detailKegiatan,
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(right: 16, left: 16, top: 30),
+                child: Text(
+                  "Map:",
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(right: 16, left: 16, top: 2),
+                child: Text(
+                  "Map not found",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+}
---
+++ lib/service/paymentDetailService.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:wedding_planner/model/paymentDetailModel.dart';
+
+class PaymentDetailService {
+  static const String _baseUrl = 'http://192.168.1.5:8000/api/';
+  // static const String _baseUrl = 'http://192.168.1.5:8000/api/';
+  // static const String _baseUrl = 'http://192.168.43.110:8000/api/';
+
+  var tokenData = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Accept': "application/json"
+  };
+
+  Future<PaymentDetail> getAllPaymentDetail() async {
+    final response = await http.get(Uri.parse(_baseUrl + "payment_detail"));
+    if (response.statusCode == 200) {
+      return PaymentDetail.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  Future createPaymentDetail(Map<String, dynamic> body) async {
+    final response = await http.post(Uri.parse(_baseUrl + "payment_detail"),
+        body: body, headers: tokenData);
+
+    try {
+      if (response.statusCode == 201) {
+        return true;
+      } else {
+        throw ("fail");
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<bool> deletePaymentDetail(int id) async {
+    final response = await http.delete(
+        Uri.parse(_baseUrl + "payment_detail/" + id.toString()),
+        headers: tokenData);
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> updatePaymentDetail(Map<String, dynamic> body, int id) async {
+    final response = await http.put(
+        Uri.parse(_baseUrl + "payment_detail/" + id.toString()),
+        body: body,
+        headers: tokenData);
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      // return false;
+      throw ("false");
+    }
+  }
+}

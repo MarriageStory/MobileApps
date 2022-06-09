5736cac7ffb06e27e571c624ee3e2cfe262ea3f2
efriza <Efrizals@it.student.pens.ac.id>
2022-06-09 10:22:20 +0700

    integrasi backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
-
+//navbar
 import 'package:wedding_planner/navbar/navbar.dart';
-import 'package:wedding_planner/screens/homePage/homePage.dart';
-import 'package:wedding_planner/screens/payment/payment_detail.dart';
-import 'package:wedding_planner/screens/payment/payment_screen.dart';
-
+//sign up & sign in
 import 'package:wedding_planner/screens/signin/signin_screen.dart';
 import 'package:wedding_planner/screens/signup/signup_screen.dart';
+//splash
 import 'package:wedding_planner/screens/splash/splash_screen.dart';
-import 'package:wedding_planner/screens/teams/teams_screen.dart';
+//welcome
 import 'package:wedding_planner/screens/welcome/welcome_screen.dart';
-
+//task
 import 'package:wedding_planner/screens/task/task_screen.dart';
 import 'package:wedding_planner/screens/task/task_form.dart';
 import 'package:wedding_planner/screens/task/task_detail.dart';
+//payment
+import 'package:wedding_planner/screens/homePage/homePage.dart';
+import 'package:wedding_planner/screens/payment/payment_detail.dart';
+import 'package:wedding_planner/screens/payment/payment_screen.dart';
+//teams
+import 'package:wedding_planner/screens/teams/teams_screen.dart';
 
 void main() {
   runApp(const MyApp());

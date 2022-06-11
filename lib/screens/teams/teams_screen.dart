8f808f0f84f76120a723d7a9b01b9ff368022996
Afnan Amiludin <amldn.afnn@gmail.com>
2022-06-11 21:20:23 +0700

    page teams BE to BE

--- lib/screens/teams/teams_screen.dart
+++ lib/screens/teams/teams_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:wedding_planner/screens/teams/components/body.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:wedding_planner/model/teams.dart';
+import 'package:wedding_planner/screens/teams/components/background.dart';
+import 'package:wedding_planner/screens/teams/create_teams.dart';
 
 class TeamScreen extends StatelessWidget {
   static const routeName = '/teams';
@@ -7,9 +10,162 @@ class TeamScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Teams team = ModalRoute.of(context)!.settings.arguments as Teams;
     return Scaffold(
-      // backgroundColor: Colors.green,
-      body: Body(),
+      body: SingleChildScrollView(
+        child: Background(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Positioned(
+                top: 55,
+                left: 20,
+                child: Row(
+                  children: [
+                    // Icon(
+                    //   Icons.menu,
+                    //   color: Colors.white,
+                    //   size: 35,
+                    // ),
+                    Image.asset(
+                      'assets/icons/menu.png',
+                    ),
+                    SizedBox(
+                      width: 107,
+                    ),
+                    Text(
+                      "Teams",
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 107,
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => CreateTeams()),
+                          );
+                        },
+                        icon: const Icon(Icons.add, color: Colors.white))
+                  ],
+                ),
+              ),
+              Positioned(
+                top: 150,
+                child: Column(
+                  children: [
+                    Container(
+                      height: 65,
+                      width: 325,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.white,
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 0.1,
+                              offset: Offset(0, 0), // Shadow position
+                            ),
+                          ],
+                        ),
+                        child: Stack(
+                          children: [
+                            Positioned(
+                                top: 22,
+                                left: 30,
+                                child: Text(
+                                  "Project Leader",
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                )),
+                            Positioned(
+                              top: 22,
+                              right: 50,
+                              child: Image.asset(
+                                'assets/icons/people.png',
+                              ),
+                            ),
+                            Positioned(
+                                top: 22,
+                                right: 25,
+                                child: Text(
+                                  team.leader,
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 16,
+                                    color: Color(0xFFFA5D76),
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                )),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      height: 65,
+                      width: 325,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.white,
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 0.1,
+                              offset: Offset(0, 0), // Shadow position
+                            ),
+                          ],
+                        ),
+                        child: Stack(
+                          children: [
+                            Positioned(
+                                top: 22,
+                                left: 30,
+                                child: Text(
+                                  "Staff",
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                )),
+                            Positioned(
+                              top: 22,
+                              right: 50,
+                              child: Image.asset(
+                                'assets/icons/people.png',
+                              ),
+                            ),
+                            Positioned(
+                                top: 22,
+                                right: 25,
+                                child: Text(
+                                  team.staf,
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 16,
+                                    color: Color(0xFFFA5D76),
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                )),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       bottomNavigationBar: BottomAppBar(),
     );
   }

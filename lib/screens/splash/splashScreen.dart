412e2bb89f3fd6304beaf2c0344823789a95f043
AinulM02 <janulm02@gmail.com>
2022-08-28 17:49:22 +0700

    test

--- lib/screens/splash/splashScreen.dart
+++ lib/screens/splash/splashScreen.dart
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(
-      Duration(seconds: 2),
+      const Duration(seconds: 2),
       () {
         checkToken();
       },
